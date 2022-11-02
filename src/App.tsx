11459664f560a4e2c749e54c15a2d8794e9a9710
will-k-minor <willkminor@gmail.com>
2022-11-01 23:46:12 -0500

    Fixed HMR

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Hello World</h1>
+      <h1>Vite + React.js</h1>
       <div className="card">
         <button onClick={() => setCount((count: number) => count + 1)}>
           count is {count}
